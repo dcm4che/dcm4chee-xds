b1b533adf322e64d167496a50c97275202095539
romank-agfa <roman.khazankin@agfa.com>
2015-01-20 17:20:00 +0100

    [XDSB-133] Default auto-configurator | hl7, random UIDs

--- dcm4chee-xds2-conf/src/main/java/org/dcm4chee/xds2/conf/DefaultConfigurator.java
+++ dcm4chee-xds2-conf/src/main/java/org/dcm4chee/xds2/conf/DefaultConfigurator.java
@@ -1,30 +1,35 @@
 package org.dcm4chee.xds2.conf;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.dcm4che3.conf.api.ConfigurationAlreadyExistsException;
 import org.dcm4che3.conf.api.ConfigurationException;
-import org.dcm4che3.conf.api.ConfigurationNotFoundException;
 import org.dcm4che3.conf.api.DicomConfiguration;
-import org.dcm4che3.data.Code;
-import org.dcm4che3.data.Issuer;
+import org.dcm4che3.net.Connection;
 import org.dcm4che3.net.Device;
-import org.dcm4chee.storage.conf.Availability;
+import org.dcm4che3.net.audit.AuditLogger;
+import org.dcm4che3.net.audit.AuditRecordRepository;
+import org.dcm4che3.net.hl7.HL7Application;
+import org.dcm4che3.net.hl7.HL7DeviceExtension;
+import org.dcm4che3.util.UIDUtils;
+import org.dcm4chee.storage.conf.StorageAvailability;
 import org.dcm4chee.storage.conf.Filesystem;
 import org.dcm4chee.storage.conf.FilesystemGroup;
 import org.dcm4chee.storage.conf.StorageConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DefaultConfigurator {
 
     private static final Logger log = LoggerFactory.getLogger(DefaultConfigurator.class);
 
-    private static final String[] affinityDomain = { "1.2.3.4.5" };
-    private static final String[] MIME_TYPES2 = new String[] { "application/xml", "application/dicom", "application/pdf", 
-        "application/msword", "application/msexcel", "text/plain", "text/xml", "image/jpeg", "image/png", "image/tiff" };
-    
+    private static final String[] MIME_TYPES2 = new String[]{"application/xml", "application/dicom", "application/pdf",
+            "application/msword", "application/msexcel", "text/plain", "text/xml", "image/jpeg", "image/png", "image/tiff"};
+
     public static void applyDefaultConfig(DicomConfiguration config, String deviceName) {
 
         log.info("Initializing the default configuration for device {}", deviceName);
@@ -33,29 +38,54 @@ public class DefaultConfigurator {
 
             // should work with agility
             String ip = System.getProperty("jboss.bind.address", "localhost");
-            
+
+
+            boolean useArr = false;
+            try {
+                // ARR
+                Device arr = new Device();
+                arr.setDeviceName("syslog");
+                List<Connection> arrConnections = new ArrayList<>();
+                Connection e = new Connection();
+                e.setCommonName("audit-udp");
+                e.setProtocol(Connection.Protocol.SYSLOG_UDP);
+                e.setPort(514);
+                e.setHostname(ip);
+                arrConnections.add(e);
+                arr.setConnections(arrConnections);
+
+                AuditRecordRepository ext = new AuditRecordRepository();
+                ext.setConnections(arrConnections);
+                arr.addDeviceExtension(ext);
+
+                config.persist(arr);
+            } catch (ConfigurationAlreadyExistsException cee) {
+                //noop, thats ok
+            } catch (Exception e) {
+                throw e;
+            }
+
 
             Device device = new Device(deviceName);
 
             // registry
             XdsRegistry registry = new XdsRegistry();
             device.addDeviceExtension(registry);
-            registry.setApplicationName("XDSREGISTRY");
-            registry.setAffinityDomain(affinityDomain);
+            registry.setApplicationName("XDS-REGISTRY");
+
+            // affinity domain
+            String generatedAffDomain = UIDUtils.createUID();
+            String[] affinityDomains = new String[1];
+            affinityDomains[0] = generatedAffDomain;
+            registry.setAffinityDomain(affinityDomains);
+
             registry.setAffinityDomainConfigDir("${jboss.server.config.dir}/xds/affinitydomain");
             registry.setAcceptedMimeTypes(MIME_TYPES2);
             registry.setSoapLogDir("../standalone/log/xds");
-            registry.setCreateMissingPIDs(true);
-            registry.setCreateMissingCodes(true);
-            registry.setCheckAffinityDomain(true);
-            registry.setCheckMimetype(true);
-            registry.setQueryUrl("http://"+ip+":8080/xds/registry");
-            registry.setRegisterUrl("http://"+ip+":8080/xds/registry");
-            registry.setCheckAffinityDomain(false);
-            registry.setCheckMimetype(false);
+            registry.setQueryUrl("https://" + ip + ":443/xds/registry");
+            registry.setRegisterUrl("https://" + ip + ":443/xds/registry");
 
             // generic source
-
             XdsSource source = new XdsSource();
             device.addDeviceExtension(source);
             source.setUid("0");
@@ -66,14 +96,13 @@ public class DefaultConfigurator {
             XdsRepository rep = new XdsRepository();
             device.addDeviceExtension(rep);
             rep.setApplicationName("XDS-REPOSITORY");
-            rep.setRepositoryUID("9.9.9.9");
-            rep.setRetrieveUrl("http://"+ip+":8080/xds/repository");
-            rep.setProvideUrl("http://"+ip+":8080/xds/repository");
+            rep.setRepositoryUID(UIDUtils.createUID());
+            rep.setRetrieveUrl("https://" + ip + ":443/xds/repository");
+            rep.setProvideUrl("https://" + ip + ":443/xds/repository");
             rep.setAcceptedMimeTypes(MIME_TYPES2);
             rep.setSoapLogDir("../standalone/log/xds");
             rep.setCheckMimetype(false);
-            rep.setAllowedCipherHostname("*");
-            HashMap<String,String> fsGrps = new HashMap<String,String>(1);
+            HashMap<String, String> fsGrps = new HashMap<String, String>(1);
             fsGrps.put("*", "XDS_ONLINE");
             rep.setFsGroupIDbyAffinity(fsGrps);
 
@@ -82,26 +111,65 @@ public class DefaultConfigurator {
             device.addDeviceExtension(store);
             store.setApplicationName("XDS-REPOSITORY-STORAGE");
             FilesystemGroup grp = new FilesystemGroup("XDS_ONLINE", "1GiB");
-            
+
             String serverHomeDir = System.getProperty("jboss.server.base.dir");
             File fsDir = new File(serverHomeDir, "xds-repository-storage");
-            Filesystem fs1 = new Filesystem("xds_fs_1", fsDir.toURI().toString(), 10, Availability.ONLINE);
+            Filesystem fs1 = new Filesystem("xds_fs_1", fsDir.toURI().toString(), 10, StorageAvailability.ONLINE);
             grp.addFilesystem(fs1);
             store.addFilesystemGroup(grp);
-            
-            // used elsewhere as well
+
             Map<String, Device> deviceBySrcUid = new HashMap<String, Device>();
             deviceBySrcUid.put("*", device);
             rep.setSrcDevicebySrcIdMap(deviceBySrcUid);
 
-            try {
-                if (config.findDevice(deviceName) != null)
-                    config.merge(device);
-            } catch (ConfigurationNotFoundException nfe) {
-                config.persist(device);
-            } catch (ConfigurationException ce) {
-                throw ce;
-            }
+
+            List<Connection> conns = new ArrayList<>();
+            // audit upd
+            Connection auditConn = new Connection();
+            auditConn.setProtocol(Connection.Protocol.SYSLOG_UDP);
+            auditConn.setCommonName("audit-udp");
+            auditConn.setHostname(ip);
+            conns.add(auditConn);
+
+            // hl7
+            Connection hl7conn = new Connection();
+            hl7conn.setProtocol(Connection.Protocol.HL7);
+            hl7conn.setCommonName("hl7-conn");
+            hl7conn.setHostname(ip);
+            hl7conn.setPort(2575);
+            conns.add(hl7conn);
+
+            device.setConnections(conns);
+
+            // HL7 extensions
+            HL7DeviceExtension hl7ext = new HL7DeviceExtension();
+            HL7Application hl7App = new HL7Application();
+            hl7App.setApplicationName("XDS^IMPAX");
+            ArrayList<Connection> hl7conns = new ArrayList<Connection>();
+            hl7conns.add(hl7conn);
+            hl7App.setConns(hl7conns);
+            hl7App.setAcceptedMessageTypes(
+                "ADT^A03",
+                "ADT^A06",
+                "ADT^A07",
+                "ADT^A08",
+                "ADT^A40",
+                "ORM^O01");
+
+            hl7ext.addHL7Application(hl7App);
+            device.addDeviceExtension(hl7ext);
+
+            // AuditLogger
+            AuditLogger auditLogger = new AuditLogger();
+            ArrayList<Connection> alconns = new ArrayList<Connection>();
+            alconns.add(auditConn);
+            auditLogger.setConnections(alconns);
+            auditLogger.setSchemaURI("http://www.dcm4che.org/DICOM/audit-message.rnc");
+            // leave blank, so by default there is no logging
+            //auditLogger.setAuditRecordRepositoryDevice();
+            device.addDeviceExtension(auditLogger);
+
+            config.persist(device);
 
             config.close();
 
@@ -110,6 +178,7 @@ public class DefaultConfigurator {
         }
 
     }
+
 }
 
 
