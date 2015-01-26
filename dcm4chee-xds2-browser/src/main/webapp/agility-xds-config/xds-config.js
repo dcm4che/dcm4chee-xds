950fabd3f2853d79f4dfd78d68b856c2ad598e14
romank-agfa <roman.khazankin@agfa.com>
2015-01-26 19:52:57 +0100

    [LIB-260] Advanced Configuration GUI | custom xds view

--- dcm4chee-xds2-browser/src/main/webapp/agility-xds-config/xds-config.js
+++ dcm4chee-xds2-browser/src/main/webapp/agility-xds-config/xds-config.js
@@ -9,6 +9,7 @@ angular.module('com.agfa.agility.xdsConfig', ['dcm4che.config.manager'])
             if ($scope.xdsDevice)
                 $scope.loadDeviceConfig($scope.xdsDevice, function () {
                     $scope.tab = 'registry';
+
                     try {
                         $scope.xdsHL7AppName = _.keys($scope.xdsDevice.config.deviceExtensions.HL7DeviceExtension.hl7Apps)[0];
                     } catch (e) {
@@ -19,11 +20,22 @@ angular.module('com.agfa.agility.xdsConfig', ['dcm4che.config.manager'])
 
         });
 
+
+        $scope.$watchCollection('xdsDevice.config.deviceExtensions.StorageConfiguration.filesystemGroups', function () {
+            if ($scope.xdsDevice.config) {
+                var groups = _.values($scope.xdsDevice.config.deviceExtensions.StorageConfiguration.filesystemGroups);
+                if (groups.length>0)
+                    $scope.selectedFileSystemGroup = groups[0];
+            }
+        });
+
         $scope.$watchCollection('xdsDevice.config.dicomConnection', function () {
             if (!$scope.xdsDevice || !$scope.xdsDevice.config) $scope.hl7Connection = null;
             else
                 $scope.hl7Connection = _.findWhere($scope.xdsDevice.config.dicomConnection, {cn: 'hl7-conn'})
         });
+
+
     }).directive('staticInclude', function ($http, $templateCache, $compile) {
         return function (scope, element, attrs) {
             var templatePath = attrs.staticInclude;
