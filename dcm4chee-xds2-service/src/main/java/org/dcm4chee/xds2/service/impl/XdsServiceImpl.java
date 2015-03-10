71f4c24056d7ad4820693f510a35dd4697eadbf0
romank-agfa <roman.khazankin@agfa.com>
2015-03-10 16:19:56 +0100

    [#XDS-207] Integration into Agility | startup issue quickfix - retries to 300, parameterized

--- dcm4chee-xds2-service/src/main/java/org/dcm4chee/xds2/service/impl/XdsServiceImpl.java
+++ dcm4chee-xds2-service/src/main/java/org/dcm4chee/xds2/service/impl/XdsServiceImpl.java
@@ -181,9 +181,12 @@ public class XdsServiceImpl implements XdsService {
 
                 // Ugly, but works.
                 // The issue seems to be correlated to late webservice start (thus fails if webservice does not start until now),
-                // therefore it will most likely succeed once webservice is up
+                // therefore it will most likely succeed once webservice is up.
+                //
+                // Latest observation: failed after 10 retries,
+                // but before "JBAS015539: Starting service jboss.ws.endpoint."xds-registry.ear"."dcm4chee-xds2-registry-ws-2.0.6.v20150303_1255.jar".XDSRegistryBeanWS"
 
-                int retries = 10;
+                int retries = Integer.valueOf(System.getProperty("org.dcm4chee.xds.hl7IteratorRetriesSec", "300"));
                 Iterator<HL7Service> hl7ServiceIterator = null;
                 while (retries>0)
                     try {
