d55aca07148d55ed55e75ca876584072c6046863
romank-agfa <roman.khazankin@agfa.com>
2015-03-18 09:32:17 +0100

    Revert "[#XDS-207] Integration into Agility | retry for configuration load"
    This reverts commit fe11a9d953108e6ae3df96f298a3466da86307f9.

--- dcm4chee-xds2-service/src/main/java/org/dcm4chee/xds2/service/impl/XdsServiceImpl.java
+++ dcm4chee-xds2-service/src/main/java/org/dcm4chee/xds2/service/impl/XdsServiceImpl.java
@@ -149,25 +149,18 @@ public class XdsServiceImpl implements XdsService {
         String deviceName = System.getProperty(deviceNameProperty);
         if (deviceName == null)
             deviceName = System.getProperty(DEVICE_NAME_PROPERTY, DEF_DEVICE_NAME);
+        try {
+            conf.sync();
+            return conf.findDevice(deviceName);
+        } catch (ConfigurationNotFoundException e) {
 
-        int retries = 10;
-        Device xdsDevice = null;
-
-        while (retries>0) {
-            try {
-                conf.sync();
-                xdsDevice = conf.findDevice(deviceName);
-                break;
-            } catch (ConfigurationNotFoundException e) {
-                // Try to initialize the default config if configured by a system property
-                if (System.getProperty(DO_DEFAULT_CONFIG_PROPERTY) != null)
-                    defaultConfigurator.applyDefaultConfig(deviceName);
-            }
-            retries--;
+            // Try to initialize the default config if configured by a system property
+            if (System.getProperty(DO_DEFAULT_CONFIG_PROPERTY) != null) {
+                defaultConfigurator.applyDefaultConfig(deviceName);
+                return conf.findDevice(deviceName);
+            } else
+                throw e;
         }
-
-        if (xdsDevice == null) throw new ConfigurationNotFoundException("Could not retrieve the XDS device '"+deviceName+"'");
-        return xdsDevice;
     }
 
 
